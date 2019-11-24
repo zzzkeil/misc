14c486316866ba504d57bf6e2197f303f646e29c
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-24 20:47:35 +0100

    Update base_server_setup.sh

--- base_server_setup.sh
+++ base_server_setup.sh
@@ -23,6 +23,16 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 
+if [[ -e /etc/debian_version ]]; then
+    VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
+    if [[ "$VERSION_ID" = 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="10"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="18.04"' ]]; then
+	echo "OS supported"
+      fi
+      else
+      echo "Sorry, your OS is not supported. Only Debian 9 - 10 or Ubuntu 18.04"
+      exit 1
+fi
+
 #
 # APT
 #
