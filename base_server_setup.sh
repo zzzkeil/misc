3d154e52a500b073d73339999c3b4a137b316f35
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-25 19:56:07 +0100

    Update base_server_setup.sh

--- base_server_setup.sh
+++ base_server_setup.sh
@@ -162,10 +162,6 @@ runtime2=$(uptime -p)
 totalban1=$(zgrep 'Ban' /var/log/fail2ban.log* | wc -l)
 echo "System uptime : $runtime1  / $runtime2 "
 echo ""
-stamp="/var/lib/update-notifier/updates-available"
-
-[ ! -r "$stamp" ] || cat "$stamp"
-echo ""
 echo "Total banned IPs from fail2ban : $totalban1 "
 ' >> /etc/update-motd.d/99-base01
 chmod +x /etc/update-motd.d/99-base01
