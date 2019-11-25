e3229406b2934351b2e0f996213bc028fddd8b24
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-25 19:10:39 +0100

    Update testos.sh

--- testos.sh
+++ testos.sh
@@ -7,7 +7,7 @@ if [[ -e /etc/debian_version ]]; then
 fi
 VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
 
-echo "$VERSION_ID""
+echo "$VERSION_ID"
 
 if [[ "$VERSION_ID" = 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="10"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="18.04"' ]]; then
       echo "OS supported"
