bcac24d0fbf19b70b00688f4a9a5172fc22e02d8
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-25 19:40:35 +0100

    Update base_server_setup.sh

--- base_server_setup.sh
+++ base_server_setup.sh
@@ -2,7 +2,7 @@
 clear
 echo " #############################################"
 echo " # Setup server config Netcup Ubuntu 18.04   #"
-echo " # Setup server config Netcup Debian 10 test #"
+echo " # Setup server config Netcup Debian 10      #"
 echo " # passwd,ssh,fail2ban,ufw,network,updates   #"
 echo " # !!!!!!!!!! Automatic reboot !!!!!!!!!!!   #"
 echo " #############################################"
@@ -26,14 +26,7 @@ fi
 if [[ -e /etc/debian_version ]]; then
       echo "Debian Distribution"
       else
-      echo "This is not a Debian Distribution. Only Debian 9 - 10 or Ubuntu 18.04 supported"
-fi
-
-VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
-if [[ "$VERSION_ID" = 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="10"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="18.04"' ]]; then
-      echo "OS supported"
-      else
-      echo "Sorry, your Debian/Ubuntu Version is not supported. Only Debian 9 - 10 or Ubuntu 18.04"
+      echo "This is not a Debian Distribution."
       exit 1
 fi
 
