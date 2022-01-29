4f29f27811d51221024d958ce8b7beb51a1262bc
Klaus <38099918+zzzkeil@users.noreply.github.com>
2022-01-29 16:11:42 +0100

    Update base_pi_setup_test.sh

--- base_pi_setup_test.sh
+++ base_pi_setup_test.sh
@@ -168,12 +168,12 @@ nano /etc/dhcpcd.conf
 #
 
 echo -e "${GREEN}Set ufw config  ${ENDCOLOR}"
-read -p "Allow ssh from Private IP Address or Range - CIDR" -e -i 192.168.0.0/16 cidr
+read -p "Allow ssh from Private IP Address or Range - CIDR : " -e -i 192.168.0.0/16 cidr
 ufw default deny incoming
-ufw allwo from $cidr to any port $sshport/tcp
-#ufw allwo from 192.168.0.0/16 to any port $sshport/tcp
-#ufw allwo from 172.16.0.0/12 to any port $sshport/tcp
-#ufw allwo from 10.0.0.0/8 to any port $sshport/tcp
+ufw allow from $cidr to any port $sshport/tcp
+#ufw allow from 192.168.0.0/16 to any port $sshport/tcp
+#ufw allow from 172.16.0.0/12 to any port $sshport/tcp
+#ufw allow from 10.0.0.0/8 to any port $sshport/tcp
 clear
 
 #
